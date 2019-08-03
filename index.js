dffef6c62e04190cfb7b341a91061f099a7519d5
Evan  Markert <evanmarkert@gmail.com>
2019-08-03 22:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyandValue() {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+function deleteFromDriverByKey() {
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
